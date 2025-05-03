e0b5ee2694b4c7067244db9f576821da346a4782
seojincho77 <choseo0215@naver.com>
2025-05-03 22:46:27 +0900

    Update UserService.java

--- lostitem/src/main/java/com/example/lostitem/services/UserService.java
+++ lostitem/src/main/java/com/example/lostitem/services/UserService.java
@@ -1,4 +1,33 @@
-package com.example.lostitem.services;
+package com.example.demo.services;
 
+import com.example.demo.models.Users;
+import com.example.demo.repositories.UsersRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UsersRepository usersRepository;
+
+    public UserService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public Optional<Users> getUserById(Integer id) {
+        return usersRepository.findById(id);
+    }
+
+    public Users saveUser(Users user) {
+        return usersRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        usersRepository.deleteById(id);
+    }
 }
