a46cedd58c8bea1757e619edc7cc6d6da4cd9b1a
seojincho77 <choseo0215@naver.com>
2025-05-03 22:44:51 +0900

    Update CommentService.java

--- lostitem/src/main/java/com/example/lostitem/services/CommentService.java
+++ lostitem/src/main/java/com/example/lostitem/services/CommentService.java
@@ -1,4 +1,36 @@
-package com.example.lostitem.services;
+package com.example.demo.services;
 
+import com.example.demo.models.Comment;
+import com.example.demo.models.Post;
+import com.example.demo.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+//Repository는 데이터 입출구 (저장소)
+//Service는 처리실 (데이터를 요리하는 곳)
+@Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getCommentsByPost(Post post) {
+        return commentRepository.findByPostOrderByCreatedAtDesc(post);
+    }
+
+    public Comment saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public Optional<Comment> getCommentById(Integer id) {
+        return commentRepository.findById(id);
+    }
+
+    public void deleteComment(Integer id) {
+        commentRepository.deleteById(id);
+    }
 }
+
