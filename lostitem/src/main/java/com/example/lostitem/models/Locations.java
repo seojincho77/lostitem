ad3de5bdecd49c7504c6470a774956314fb8bcb8
seojincho77 <choseo0215@naver.com>
2025-05-03 16:48:55 +0900

    Update Locations.java

--- lostitem/src/main/java/com/example/lostitem/models/Locations.java
+++ lostitem/src/main/java/com/example/lostitem/models/Locations.java
@@ -1,36 +1,22 @@
-//package com.example.lostitem.models;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-////장소
-//@Entity
-//public class Locations {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id; // 장소 ID
-//
-//    private String name; // 장소 이름
-//
-//    // 기본 생성자
-//    public Locations() {
-//    }
-//
-//    // getter와 setter 추가
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//}
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "locations")
+public class Locations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer locationId;
+
+    private String locName;
+
+    private Boolean isActive;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "location")
+    private List<Post> posts;
+}
