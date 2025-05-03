aa8eea89fd9b53f3184681be98a9c921d29811b1
seojincho77 <choseo0215@naver.com>
2025-05-03 16:50:20 +0900

    Update Post.java

--- lostitem/src/main/java/com/example/lostitem/models/Post.java
+++ lostitem/src/main/java/com/example/lostitem/models/Post.java
@@ -1,24 +1,46 @@
-//package com.example.lostitem.models;
-////게시글 모델
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//
-//import java.time.LocalDateTime;
-//
-//
-//@Entity
-//public class Post {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;  // 게시글 ID
-//
-//    private String title;  // 게시글 제목
-//    private String description;  // 게시글 설명
-//    private LocalDateTime foundTime; //발견된 시간
-//
-//    // 기본 생성자, getter, setter
-//
-//
-//}
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private PostType postType;  // ENUM('lost', 'found')
+
+    private String storageLocationId;
+
+    private String foundPlace;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id2")
+    private Locations location;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+
+    @OneToOne(mappedBy = "post")
+    private LostItem lostItem;
+
+    @OneToMany(mappedBy = "post")
+    private List<Request> requests;
+}
