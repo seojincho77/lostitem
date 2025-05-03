71848e4d9a7d6d97eebf9bf8572b843943973a9c
seojincho77 <choseo0215@naver.com>
2025-05-03 16:46:38 +0900

    Update Comment.java

--- lostitem/src/main/java/com/example/lostitem/models/Comment.java
+++ lostitem/src/main/java/com/example/lostitem/models/Comment.java
@@ -1,59 +1,25 @@
-//package com.example.lostitem.models;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.ManyToOne;
-//
-//@Entity
-//public class Comment {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id; // 댓글 ID
-//
-//    private String content; // 댓글 내용
-//
-//    @ManyToOne
-//    private Users user; // 댓글 작성자 (사용자)
-//
-//    @ManyToOne
-//    private Post post; // 댓글이 달린 게시글
-//
-//    // 기본 생성자
-//    public Comment() {
-//    }
-//
-//    // getter와 setter 추가
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public Users getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(Users user) {
-//        this.user = user;
-//    }
-//
-//    public Post getPost() {
-//        return post;
-//    }
-//
-//    public void setPost(Post post) {
-//        this.post = post;
-//    }
-//}
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
