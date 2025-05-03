2652143fb31a038ff45f74b63998e7a45306cdbb
seojincho77 <choseo0215@naver.com>
2025-05-03 16:51:05 +0900

    Update Users.java

--- lostitem/src/main/java/com/example/lostitem/models/Users.java
+++ lostitem/src/main/java/com/example/lostitem/models/Users.java
@@ -1,54 +1,31 @@
-//package com.example.lostitem.models;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
+package com.example.lostitem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 ////사용자 모델
-//@Entity
-//public class Users {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id; // 사용자 ID
-//
-//    private String username; // 사용자 이름
-//    private String password; // 사용자 비밀번호
-//    private String email; // 사용자 이메일
-//
-//    // 기본 생성자
-//    public void User() {
-//    }
-//
-//    // getter와 setter 추가
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//}
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    private Boolean isAdmin;
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "user")
+    private List<Request> requests;
+
+    @OneToMany(mappedBy = "admin")
+    private List<Request> approvedRequests;
+}
