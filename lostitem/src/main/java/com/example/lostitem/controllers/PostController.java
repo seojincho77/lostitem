7b75c0fa526a80fe7965d62229c664bf149904fa
chunys <davidchun18@gmail.com>
2025-05-04 17:53:12 +0900

    update application.properties

--- lostitem/src/main/java/com/example/lostitem/controllers/PostController.java
+++ lostitem/src/main/java/com/example/lostitem/controllers/PostController.java
@@ -1,4 +1,18 @@
 package com.example.lostitem.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+    @GetMapping("/lost-items")
+    public String lostItems() {
+        return "form";
+    }
+
+    @GetMapping("/found-items")
+    public String foundItems() {
+        return "form";
+    }
+
 }
