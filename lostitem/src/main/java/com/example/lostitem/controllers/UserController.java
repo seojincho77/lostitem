b32b552f7a44f8eef0c29ebed350cc410a61f73e
chunys <davidchun18@gmail.com>
2025-05-07 14:23:41 +0900

    update login,register with frontend integration

--- lostitem/src/main/java/com/example/lostitem/controllers/UserController.java
+++ lostitem/src/main/java/com/example/lostitem/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     @GetMapping("/register")
     public String register() {
-        return "register";
+        return "signup";
     }
 
     @GetMapping("/login")
