29bfa3ba9473c9e6677020e2d8a54a419cb45a66
chunys <davidchun18@gmail.com>
2025-05-17 23:48:03 +0900

    add lostitem Crud

--- lostitem/src/main/java/com/example/lostitem/controllers/LostController.java
+++ lostitem/src/main/java/com/example/lostitem/controllers/LostController.java
@@ -1,17 +1,29 @@
 package com.example.lostitem.controllers;
 
+import com.example.lostitem.models.LostItem;
+import com.example.lostitem.models.Post;
+import com.example.lostitem.models.PostType;
 import com.example.lostitem.models.Users;
+import com.example.lostitem.services.LostItemService;
+import com.example.lostitem.services.PostService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/lost-items")
 public class LostController {
+    private final PostService postService;
+    private final LostItemService lostItemService;
+
+    public LostController(PostService postService, LostItemService lostItemService) {
+        this.postService = postService;
+        this.lostItemService = lostItemService;
+    }
+
     @GetMapping
     public String lostItems(HttpSession session, Model model) {
         Users user = (Users) session.getAttribute("user");
@@ -27,7 +39,31 @@ public class LostController {
     }
 
     @PostMapping("/new")
-    public String CreateLostItem(@ModelAttribute("user") Users user, Model model) {
-        return "lost_list";
+    public String CreateLostItem(
+            @RequestParam("title") String title,
+            @RequestParam("lostPlace") String lostPlace,
+            @RequestParam("lostDate") LocalDate lostDate,
+            @RequestParam("description") String description,
+            HttpSession session,
+            Model model) {
+        Users user = (Users) session.getAttribute("user");
+        model.addAttribute("user", user);
+
+        Post post = new Post();
+        post.setTitle(title);
+        post.setDescription(description);
+        post.setUser(user);
+        post.setPostType(PostType.lost);
+
+
+        LostItem lostitem = new LostItem();
+        lostitem.setLostPlace(lostPlace);
+        lostitem.setLostDate(lostDate);
+        lostitem.setPost(post);
+
+        post.setLostItem(lostitem);
+        postService.savePost(post);
+
+        return "redirect:/lost-items";
     }
 }
